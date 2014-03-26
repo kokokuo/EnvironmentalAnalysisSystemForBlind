f84bba26e9f0e987e8e25a78035931f7ea98f477
yi-cheng-kuo <v6610688@gmail.com>
2014-03-26 17:45:51 +0800

    3-26 加入連通抓輪廓
    Mask後加入選擇性過濾雜訊與膨脹
    加入連通效果，嘗試把連通作為偵測斑馬線的另一個判斷條件

--- ZebraCrossing_Test/ZebraCrossing_Test/Form1.Designer.cs
+++ ZebraCrossing_Test/ZebraCrossing_Test/Form1.Designer.cs
@@ -37,9 +37,17 @@
             this.toGrayButton = new System.Windows.Forms.Button();
             this.cropBottomButton = new System.Windows.Forms.Button();
             this.grayImgBox = new Emgu.CV.UI.ImageBox();
+            this.contourButton = new System.Windows.Forms.Button();
+            this.contourImageBox = new Emgu.CV.UI.ImageBox();
+            this.smoothButton = new System.Windows.Forms.Button();
+            this.dilateButton = new System.Windows.Forms.Button();
+            this.filterImageBox = new Emgu.CV.UI.ImageBox();
+            this.filterPepperButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.oriImageBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.maskImageBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.grayImgBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contourImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filterImageBox)).BeginInit();
             this.SuspendLayout();
             // 
             // oriImageBox
@@ -67,7 +75,7 @@
             this.loadImgButton.Name = "loadImgButton";
             this.loadImgButton.Size = new System.Drawing.Size(126, 35);
             this.loadImgButton.TabIndex = 5;
-            this.loadImgButton.Text = "載入圖片";
+            this.loadImgButton.Text = "1.載入圖片";
             this.loadImgButton.UseVisualStyleBackColor = true;
             this.loadImgButton.Click += new System.EventHandler(this.loadImgButton_Click);
             // 
@@ -76,20 +84,20 @@
             this.maskImgButton.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.maskImgButton.Location = new System.Drawing.Point(12, 393);
             this.maskImgButton.Name = "maskImgButton";
-            this.maskImgButton.Size = new System.Drawing.Size(126, 35);
+            this.maskImgButton.Size = new System.Drawing.Size(141, 35);
             this.maskImgButton.TabIndex = 6;
-            this.maskImgButton.Text = "Mask白色";
+            this.maskImgButton.Text = "3.Mask白色";
             this.maskImgButton.UseVisualStyleBackColor = true;
             this.maskImgButton.Click += new System.EventHandler(this.maskImgButton_Click);
             // 
             // houghLineButton
             // 
             this.houghLineButton.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.houghLineButton.Location = new System.Drawing.Point(521, 26);
+            this.houghLineButton.Location = new System.Drawing.Point(723, 26);
             this.houghLineButton.Name = "houghLineButton";
             this.houghLineButton.Size = new System.Drawing.Size(220, 35);
             this.houghLineButton.TabIndex = 7;
-            this.houghLineButton.Text = "顯示HoughLine";
+            this.houghLineButton.Text = "5.顯示HoughLine";
             this.houghLineButton.UseVisualStyleBackColor = true;
             this.houghLineButton.Click += new System.EventHandler(this.houghLineButton_Click);
             // 
@@ -100,7 +108,7 @@
             this.toGrayButton.Name = "toGrayButton";
             this.toGrayButton.Size = new System.Drawing.Size(126, 35);
             this.toGrayButton.TabIndex = 8;
-            this.toGrayButton.Text = "灰階";
+            this.toGrayButton.Text = "2.灰階";
             this.toGrayButton.UseVisualStyleBackColor = true;
             this.toGrayButton.Click += new System.EventHandler(this.toGrayButton_Click);
             // 
@@ -109,9 +117,9 @@
             this.cropBottomButton.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.cropBottomButton.Location = new System.Drawing.Point(12, 67);
             this.cropBottomButton.Name = "cropBottomButton";
-            this.cropBottomButton.Size = new System.Drawing.Size(126, 35);
+            this.cropBottomButton.Size = new System.Drawing.Size(168, 35);
             this.cropBottomButton.TabIndex = 9;
-            this.cropBottomButton.Text = "剪裁下半部";
+            this.cropBottomButton.Text = "1.opt-剪裁下半部";
             this.cropBottomButton.UseVisualStyleBackColor = true;
             this.cropBottomButton.Click += new System.EventHandler(this.cropBottomButton_Click);
             // 
@@ -124,11 +132,79 @@
             this.grayImgBox.TabIndex = 10;
             this.grayImgBox.TabStop = false;
             // 
+            // contourButton
+            // 
+            this.contourButton.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.contourButton.Location = new System.Drawing.Point(938, 393);
+            this.contourButton.Name = "contourButton";
+            this.contourButton.Size = new System.Drawing.Size(126, 35);
+            this.contourButton.TabIndex = 11;
+            this.contourButton.Text = "5.連通";
+            this.contourButton.UseVisualStyleBackColor = true;
+            this.contourButton.Click += new System.EventHandler(this.contourButton_Click);
+            // 
+            // contourImageBox
+            // 
+            this.contourImageBox.Location = new System.Drawing.Point(744, 445);
+            this.contourImageBox.Name = "contourImageBox";
+            this.contourImageBox.Size = new System.Drawing.Size(320, 240);
+            this.contourImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.contourImageBox.TabIndex = 12;
+            this.contourImageBox.TabStop = false;
+            // 
+            // smoothButton
+            // 
+            this.smoothButton.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.smoothButton.Location = new System.Drawing.Point(374, 67);
+            this.smoothButton.Name = "smoothButton";
+            this.smoothButton.Size = new System.Drawing.Size(211, 35);
+            this.smoothButton.TabIndex = 13;
+            this.smoothButton.Text = "2.opt-灰階與模糊";
+            this.smoothButton.UseVisualStyleBackColor = true;
+            this.smoothButton.Click += new System.EventHandler(this.smoothButton_Click);
+            // 
+            // dilateButton
+            // 
+            this.dilateButton.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dilateButton.Location = new System.Drawing.Point(557, 393);
+            this.dilateButton.Name = "dilateButton";
+            this.dilateButton.Size = new System.Drawing.Size(137, 35);
+            this.dilateButton.TabIndex = 14;
+            this.dilateButton.Text = "4.opt-膨脹";
+            this.dilateButton.UseVisualStyleBackColor = true;
+            this.dilateButton.Click += new System.EventHandler(this.dilateButton_Click);
+            // 
+            // dilateImageBox
+            // 
+            this.filterImageBox.Location = new System.Drawing.Point(374, 445);
+            this.filterImageBox.Name = "dilateImageBox";
+            this.filterImageBox.Size = new System.Drawing.Size(320, 240);
+            this.filterImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.filterImageBox.TabIndex = 15;
+            this.filterImageBox.TabStop = false;
+            // 
+            // filterPepperButton
+            // 
+            this.filterPepperButton.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.filterPepperButton.Location = new System.Drawing.Point(374, 393);
+            this.filterPepperButton.Name = "filterPepperButton";
+            this.filterPepperButton.Size = new System.Drawing.Size(143, 35);
+            this.filterPepperButton.TabIndex = 16;
+            this.filterPepperButton.Text = "4.opt-去雜訊";
+            this.filterPepperButton.UseVisualStyleBackColor = true;
+            this.filterPepperButton.Click += new System.EventHandler(this.filterPepperButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1020, 706);
+            this.ClientSize = new System.Drawing.Size(1131, 706);
+            this.Controls.Add(this.filterPepperButton);
+            this.Controls.Add(this.filterImageBox);
+            this.Controls.Add(this.dilateButton);
+            this.Controls.Add(this.smoothButton);
+            this.Controls.Add(this.contourImageBox);
+            this.Controls.Add(this.contourButton);
             this.Controls.Add(this.grayImgBox);
             this.Controls.Add(this.cropBottomButton);
             this.Controls.Add(this.toGrayButton);
@@ -142,6 +218,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.oriImageBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.maskImageBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.grayImgBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contourImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filterImageBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -156,6 +234,12 @@
         private System.Windows.Forms.Button toGrayButton;
         private System.Windows.Forms.Button cropBottomButton;
         private Emgu.CV.UI.ImageBox grayImgBox;
+        private System.Windows.Forms.Button contourButton;
+        private Emgu.CV.UI.ImageBox contourImageBox;
+        private System.Windows.Forms.Button smoothButton;
+        private System.Windows.Forms.Button dilateButton;
+        private Emgu.CV.UI.ImageBox filterImageBox;
+        private System.Windows.Forms.Button filterPepperButton;
     }
 }
 
--- ZebraCrossing_Test/ZebraCrossing_Test/Form1.cs
+++ ZebraCrossing_Test/ZebraCrossing_Test/Form1.cs
@@ -22,7 +22,6 @@ namespace ZebraCrossing_Test
     {
         DirectoryInfo dir;
         Image<Bgr, byte> oriImg;
-        Image<Bgr, byte> bottomSideImg;
         Image<Gray, byte> grayImg;
         Image<Gray, byte> maskWhiteImg;
         ImageViewer houghLineViewer;
@@ -88,7 +87,7 @@ namespace ZebraCrossing_Test
                 try
                 {
                     maskWhiteImg = new Image<Gray, byte>(new Size(grayImg.Width, grayImg.Height));
-                    CvInvoke.cvInRangeS(grayImg, new MCvScalar(200, 200, 200), new MCvScalar(255, 255, 255), maskWhiteImg);
+                    CvInvoke.cvInRangeS(grayImg, new MCvScalar(180, 180, 180), new MCvScalar(255, 255, 255), maskWhiteImg);
                     maskImageBox.Image = maskWhiteImg;
                 }
                 catch (Exception ex)
@@ -104,12 +103,22 @@ namespace ZebraCrossing_Test
             }
         }
 
+        //先灰階再模糊 以去除Mask時的雜訊白點，增加足夠有利的線段
+        private void smoothButton_Click(object sender, EventArgs e)
+        {
+            if (oriImg != null)
+            {
+                grayImg = oriImg.Convert<Gray, byte>();
+                CvInvoke.cvSmooth(grayImg, grayImg, SMOOTH_TYPE.CV_GAUSSIAN, 13, 13, 1.5, 1);
+                grayImgBox.Image = grayImg;
+            }
+        }
 
         private void toGrayButton_Click(object sender, EventArgs e)
         {
-            if (bottomSideImg != null)
+            if (oriImg != null)
             {
-                grayImg = bottomSideImg.Convert<Gray, byte>();
+                grayImg = oriImg.Convert<Gray, byte>();
                 grayImgBox.Image = grayImg;
             }
             else {
@@ -121,9 +130,9 @@ namespace ZebraCrossing_Test
         {
             if (oriImg != null)
             {
-                bottomSideImg = oriImg.Copy();
-                bottomSideImg.ROI = new Rectangle(new Point(0, 240), new Size(640, 320));
-                oriImageBox.Image = bottomSideImg;
+                oriImg = oriImg.Copy();
+                oriImg.ROI = new Rectangle(new Point(0, 240), new Size(640, 320));
+                oriImageBox.Image = oriImg;
             }
             else {
                 MessageBox.Show("尚未載入圖片");
@@ -135,35 +144,165 @@ namespace ZebraCrossing_Test
         {
             if (maskWhiteImg != null)
             {
-
-                //Hough transform for line detection
-                LineSegment2D[][] lines = maskWhiteImg.HoughLines(
-                    new Gray(125),  //Canny algorithm low threshold
-                    new Gray(260),  //Canny algorithm high threshold
-                    1,              //rho parameter
-                    Math.PI / 180.0,  //theta parameter 
-                    130,            //threshold
-                    1,             //min length for a line
-                    30);            //max allowed gap along the line
-                //draw lines on image
-                foreach (var line in lines[0])
-                {
-                    //如何限制角度http://yy-programer.blogspot.tw/2013/02/emgucv-image-process-extracting-lines_28.html
-                    //vector是向量，代表的是這個線的方向。線的點是在LineSegment2D這個結構裡的：P1與P2才是。﻿
-                    PointF vector = line.Direction;
-                    double angle = Math.Atan2(vector.Y, vector.X) * 180.0 / Math.PI;
-                    if ((angle > 160 && angle < 180) || (angle > -180 && angle < -160))
+                using (Image<Bgr, byte> showLineImg = oriImg.Copy()) {
+                    //Hough transform for line detection
+                    LineSegment2D[][] lines = maskWhiteImg.HoughLines(
+                        new Gray(125),  //Canny algorithm low threshold
+                        new Gray(260),  //Canny algorithm high threshold
+                        1,              //rho parameter
+                        Math.PI / 180.0,  //theta parameter 
+                        130,            //threshold
+                        1,             //min length for a line
+                        30);            //max allowed gap along the line
+                    //draw lines on image
+                    foreach (var line in lines[0])
                     {
-                        bottomSideImg.Draw(line, new Bgr(0, 0, 255), 2);
+                        //如何限制角度http://yy-programer.blogspot.tw/2013/02/emgucv-image-process-extracting-lines_28.html
+                        //vector是向量，代表的是這個線的方向。線的點是在LineSegment2D這個結構裡的：P1與P2才是。﻿
+                        PointF vector = line.Direction;
+                        double angle = Math.Atan2(vector.Y, vector.X) * 180.0 / Math.PI;
+                        if ((angle > 160 && angle < 190) || (angle > -190 && angle < -160))
+                        {
+                            showLineImg.Draw(line, new Bgr(0, 0, 255), 2);
+                        }
+                        Console.WriteLine("Angle = " + angle + ", P1 = " + line.P1 + ", P2 = " + line.P2 + ", length = " + line.Length);
+                        showLineImg.Draw(line, new Bgr(255, 0, 0), 1);
                     }
-                    Console.WriteLine("Angle = " + angle + ", P1 = " + line.P1 + ", P2 = " + line.P2 +", length = " +line.Length );
-                    bottomSideImg.Draw(line, new Bgr(255, 0, 0), 1);
-                }
 
-                houghLineViewer.Image = bottomSideImg;
-                houghLineViewer.Text = "HoughLine 偵測畫面";
-                houghLineViewer.Show();
+                    houghLineViewer.Image = showLineImg;
+                    houghLineViewer.Text = "HoughLine 偵測畫面";
+                    houghLineViewer.Show();
+                }
+                
             }
         }
+
+        private void contourButton_Click(object sender, EventArgs e)
+        {
+            if (maskWhiteImg != null)
+            {
+                Contour<Point> contours = DoContours(maskWhiteImg);
+                using (Image<Bgr, byte> showContoursImg = oriImg.Copy()) {
+                    //繪製所有輪廓
+                    while (contours.HNext != null)
+                    {
+                        //繪製輪廓BoundingBox
+                        showContoursImg.Draw(contours.BoundingRectangle, new Bgr(Color.Red), 2);
+                        //繪製輪廓
+                        //showContoursImg.Draw(contours, new Bgr(Color.Yellow), new Bgr(Color.GreenYellow), 1, 2);
+                        contours = contours.HNext;
+                    }
+
+                    contourImageBox.Image = showContoursImg;
+                }
+            }
+            else {
+                MessageBox.Show("尚未Mask");
+            }
+           
+        }
+
+        #region 取輪廓
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// 從圖像中取得所有的輪廓
+        /// </summary>
+        /// <param name="srcImg">來源圖像,這邊是二值化侵蝕膨脹後的圖像</param>
+        /// <returns>回傳輪廓</returns>
+        public Contour<Point> DoContours(Image<Gray, Byte> srcImg)
+        {
+            Contour<Point> objectContours = srcImg.FindContours(CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE, RETR_TYPE.CV_RETR_LIST);
+            //Contour<Point> objectContours = srcImg.FindContours();
+            return objectContours;
+        }
+        /// <summary>
+        /// 取得輪廓資料中的最大輪廓,若要取得最大輪廓的BoundingBox,使用contours.BoundingBox
+        /// </summary>
+        /// <param name="contours">輸入從圖像中取得的所有輪廓</param>
+        /// <returns>回傳最大面積的輪廓</returns>
+        public Contour<Point> GetMaxContours(Contour<Point> contours)
+        {
+            Contour<Point> MaxContour = contours;
+            while (contours.HNext != null)
+            {
+                if (MaxContour.Area < contours.HNext.Area)
+                {
+                    MaxContour = contours.HNext;
+                }
+                contours = contours.HNext;
+            }
+            return MaxContour;
+        }
+        public Image<Bgr, Byte> GetBoundingBoxImage(Contour<Point> contours, Image<Bgr, Byte> sceneImg)
+        {
+            Image<Bgr, Byte> roiImage = sceneImg.GetSubRect(contours.BoundingRectangle);
+            return roiImage;
+        }
+        /// <summary>
+        /// 劃出所有輪廓到圖像上
+        /// </summary>
+        /// <param name="contours">取得的輪廓</param>
+        /// <param name="drawImg">要畫到的圖像上</param>
+        /// <returns>回傳畫上輪廓的圖像</returns>
+        public Image<Bgr, Byte> DrawAllContoursOnImg(Contour<Point> contours, Image<Bgr, Byte> drawImg)
+        {
+            drawImg.Draw(contours, new Bgr(Color.Red), new Bgr(Color.Yellow), 1, 2);
+            return drawImg;
+        }
+        /// <summary>
+        /// 畫上最大的輪廓到圖像上
+        /// </summary>
+        /// <param name="maxContour">最大的輪廓</param>
+        /// <param name="drawImg">要畫到的圖像上</param>
+        /// <returns>回傳畫上輪廓的圖像</returns>
+        public Image<Bgr, Byte> DrawMaxContoursOnImg(Contour<Point> maxContour, Image<Bgr, Byte> drawImg)
+        {
+            drawImg.Draw(maxContour, new Bgr(Color.Red), new Bgr(Color.Yellow), 1, 2);
+            return drawImg;
+        }
+        /// <summary>
+        /// 畫上最大輪廓的BoundimgBox
+        /// </summary>
+        /// <param name="maxContour">最大的輪廓</param>
+        /// <param name="drawImg">要畫到的圖像上</param>
+        /// <returns>回傳畫上最大輪廓的BoundingBox的圖像</returns>
+        public Image<Bgr, Byte> DrawContoursMaxBoundingBoxOnImg(Contour<Point> maxContour, Image<Bgr, Byte> drawImg)
+        {
+            drawImg.Draw(maxContour.BoundingRectangle, new Bgr(Color.Red), 2);
+            return drawImg;
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        #endregion
+
+
+
+        private void dilateButton_Click(object sender, EventArgs e)
+        {
+            if (maskWhiteImg != null)
+            {
+                //膨脹
+                maskWhiteImg = maskWhiteImg.Dilate(1);
+               
+                filterImageBox.Image = maskWhiteImg;
+            }
+            else {
+                MessageBox.Show("尚未Mask");
+            }
+        }
+
+        private void filterPepperButton_Click(object sender, EventArgs e)
+        {
+            if (maskWhiteImg != null)
+            {
+                //用中值濾波去雜訊
+                maskWhiteImg = maskWhiteImg.SmoothMedian(3);
+                filterImageBox.Image = maskWhiteImg;
+            }
+            else
+            {
+                MessageBox.Show("尚未Mask");
+            }
+        }
+
     }
 }
